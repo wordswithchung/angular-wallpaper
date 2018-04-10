1f4d00f1b519617cf3558a33ed1345888626de15
Chung Nguyen <chung.nguyen@gmail.com>
2018-04-09 20:44:09 -0700

    Oh hey, let us try to adjust the path one more time :)

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -14,10 +14,10 @@ export class TopNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.instagram = ['../../../assets/if_Instagram_logo_2258498.png', 'https://www.instagram.com/wordswithchung/'];
-    this.linkedin = ['../../../assets/if_Linkedin_logo_2258497.png', 'https://www.linkedin.com/in/chungtnguyen/'];
-    this.medium = ['../../../assets/if_Medium_logo_2258488.png', 'https://medium.com/@wordswithchung'];
-    this.twitter = ['../../../assets/if_Twitter_letter_logo_2258489.png', 'https://twitter.com/wordswithchung'];
+    this.instagram = ['./assets/if_Instagram_logo_2258498.png', 'https://www.instagram.com/wordswithchung/'];
+    this.linkedin = ['./assets/if_Linkedin_logo_2258497.png', 'https://www.linkedin.com/in/chungtnguyen/'];
+    this.medium = ['./assets/if_Medium_logo_2258488.png', 'https://medium.com/@wordswithchung'];
+    this.twitter = ['./assets/if_Twitter_letter_logo_2258489.png', 'https://twitter.com/wordswithchung'];
   }
 
 }
